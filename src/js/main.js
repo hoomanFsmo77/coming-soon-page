9b32331669fdaee407b687c1b61c3f6b47c29c57
hooman mousavi <hoomanmousavi77@gmail.com>
2022-10-22 21:23:27 +0330

    header cmpleted

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
+import '../images/bg.jpg';
+import '../images/bg-dark.jpg';
 import '../style/main.scss';
+import Alpine from "alpinejs";
+import persist from '@alpinejs/persist';
+import dark_mode from "./components/dark_mode/dark_mode";
+import { init } from 'ityped';
+import "bootstrap";
+/////////////////////////////////////
+window.Alpine = Alpine
+Alpine.plugin(persist)
+Alpine.data('dark_mode',dark_mode)
+Alpine.start()
+
+const text = document.querySelector('#text')
+
+
+init(text, { showCursor: true, strings: ['dolor sit amet ipsum dolor.'] });
