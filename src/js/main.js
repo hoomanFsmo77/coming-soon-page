7115cf79deda2c9473b39d92fb50438c0763569f
hooman mousavi <hoomanmousavi77@gmail.com>
2022-10-22 23:37:26 +0330

    counter part completed

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,14 @@ import '../style/main.scss';
 import Alpine from "alpinejs";
 import persist from '@alpinejs/persist';
 import dark_mode from "./components/dark_mode/dark_mode";
+import countdown from "./components/countdown/countdown.js";
 import { init } from 'ityped';
 import "bootstrap";
 /////////////////////////////////////
 window.Alpine = Alpine
 Alpine.plugin(persist)
 Alpine.data('dark_mode',dark_mode)
+Alpine.data('countdown',countdown)
 Alpine.start()
 
 const text = document.querySelector('#text')
